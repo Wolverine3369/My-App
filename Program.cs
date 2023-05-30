b39ec7b0aabc1c605089ff3c77be4816de8022a5
Wolverine3369 <Wolverine3369@users.noreply.github.com>
2023-05-30 19:19:52 +0200

    master

--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! It`s great to see you again!");
+Console.WriteLine("Hello, master! It`s great to see you again!");
